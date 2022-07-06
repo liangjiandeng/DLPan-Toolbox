578baef2fa45dd3763ab26eb6d794a68d9f5e8f9
Xiao Wu <923071808@qq.com>
2022-07-06 21:13:45 +0800

    Update slurm_train.sh

--- 01-DL-toolbox(Pytorch)/UDL/Basis/slurm_train.sh
+++ 01-DL-toolbox(Pytorch)/UDL/Basis/slurm_train.sh
@@ -5,7 +5,7 @@ set -x
 #cd projects/derain
 
 PARTITION=defq
-JOB_NAME=rain
+JOB_NAME=task
 #CONFIG=$3
 #WORK_DIR=$4
 GPUS=${GPUS:-16}
@@ -25,7 +25,7 @@ srun -p ${PARTITION} \
     --kill-on-bad-exit=1 \
     --nodelist=node[004-005] \
     ${SRUN_ARGS} \
-    python -u dr_main.py --launcher="slurm" #${PY_ARGS}
+    python -u main.py --launcher="slurm" #${PY_ARGS}
 
 #srun -p defq -J test -n 2 --nodelist=node[004-005] --ntasks-per-node=2 --export=cuda_home python -u test_slurm.py
 #srun --partition=defq --job-name=rain -n 1 --nodelist=node004 --gres=gpu:8 --ntasks-per-node=8 python -u derain_main.py --launcher="slurm
